b5db8d3fdc66a688212a43216ad9b434b78cbbbb
REYNAUD Antony Ext OBS/CMI <HGQW0143@orange.com>
2020-04-07 16:44:14 +0200

    feat: dwarves!

---
+++ dwarves.js
@@ -0,0 +1,41 @@
+module.exports = [{
+        name: "dulrun",
+        race: "Dwarf",
+        agility: 30,
+        intellect: 10,
+        strength: 67,
+        health: 103
+    },
+    {
+        name: "florvom",
+        race: "Dwarf",
+        agility: 44,
+        intellect: 17,
+        strength: 97,
+        health: 166
+    },
+    {
+        name: "thibok",
+        race: "Dwarf",
+        agility: 30,
+        intellect: 18,
+        strength: 77,
+        health: 263
+    },
+    {
+        name: "frifem",
+        race: "Dwarf",
+        agility: 32,
+        intellect: 6,
+        strength: 107,
+        health: 196
+    },
+    {
+        name: "krober",
+        race: "Dwarf",
+        agility: 32,
+        intellect: 6,
+        strength: 54,
+        health: 236
+    }
+];
